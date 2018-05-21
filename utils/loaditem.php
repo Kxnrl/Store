c70a926d5bd463089709c4388c4b66a9be36d602
Kyle <kyle@kxnrl.com>
2018-05-22 01:16:16 +0800

    update loaditem script

--- utils/loaditem.php
+++ utils/loaditem.php
@@ -3,12 +3,6 @@
 ini_set("display_errors", 1);
 require_once(__DIR__ . "/kvphp.php");
 
-$path = __DIR__ . "/items.txt";
-
-if(!file_exists($path)){
-    die("file doesnt exists!");
-}
-
 $db_user = "";
 $db_pswd = "";
 $db_host = "";
@@ -18,6 +12,8 @@ $database = mysqli_connect($db_host, $db_user, $db_pswd, $db_name);
 
 $store = array();
 $parent = array();
+
+mysqli_query($database, "SET NAMES UTF8");   
 
 $result = mysqli_query($database, "SELECT * FROM store_item_parent");
 while($row = mysqli_fetch_array($result))
@@ -35,7 +31,19 @@ while($row = mysqli_fetch_array($result))
         if($v['id'] == $row['parent'])
             $row['catelogy'] = $v['name'];
     }
-    
+
+	foreach($row as $r => $w)
+	{
+		if(strstr($w, "ITEM_NO") == FALSE && strstr($w, "0.0 0.0 0.0") == FALSE && !is_numeric($r))
+		{
+			//echo $row['name']." -> ".$r." was found.\n";
+		}
+		else
+		{
+			//echo $row['name']." -> ".$r." was not found.\n";
+			unset($row[$r]);
+		}
+	}
     if(!isset($row['catelogy']))
         echo $row['name']." has not found parent\n";
     
@@ -64,7 +72,7 @@ while($row = mysqli_fetch_array($result))
         
         $fp = fopen( __DIR__ . "/errorlog.php", "a");
         fputs($fp, "<?PHP exit;?>    ");
-        fputs($fp, "DELETE id=".$row['id']." player_id=".$row['id']." type=".$row['type']." unique_id=".$row['unique_id']." date_of_purchase=".$row['date_of_purchase']." date_of_expration=".$row['date_of_expration']." price_of_purchase=".$row['price_of_purchase']." FROM store_items");
+        fputs($fp, "DELETE id='".$row['id']."' player_id=".$row['id']." type=".$row['type']." unique_id=".$row['unique_id']." date_of_purchase=".$row['date_of_purchase']." date_of_expration=".$row['date_of_expration']." price_of_purchase=".$row['price_of_purchase']." FROM store_items");
         fputs($fp, "\n");
         fclose($fp);
         $delet[] = $row['id'];
@@ -80,5 +88,10 @@ foreach($delet as $key => $values)
 {
     mysqli_query($database, "DELETE FROM store_items WHERE id=".$values.";");
 }
+
+print_r($parent);
+echo "\n";
+echo "\n";
+print_r($store);
 
 ?>
